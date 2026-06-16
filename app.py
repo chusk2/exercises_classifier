0f7d1a9b015c637e2377d599e04efa9f3efcd3b6
Daniel <294.daniel@gmail.com>
2026-06-16 23:50:53 +0200

    Set up app with file uploder, parse exercises and select exercise type

---
+++ app.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from pypdf import PdfReader
+import json
+
+st.set_page_config(layout = "wide", page_title = "Exam Classifier")
+st.title("Exam Classifier")
+
+TOPICS_DICT = {
+    "formulación" : "formulación",
+    "la transformación química" : "cantidad química",
+    "la estructura del átomo" : "átomo",
+    "enlaces químicos" : "enlace químico",
+    "energía de las reacciones químicas" : "termoquímica",
+    "equilibrio químico" : "equilibrio químico",
+    "equilibrios ácido-base" : "ácido base",
+    "reacciones redox" : "redox",
+    "equilibrios de precipitación" : "solubilidad",
+    "orgánica" : "reactividad orgánica"
+}
+
+file = st.file_uploader(label="Selecciona un archivo...", type = ["pdf"])
+
+def parse_text(text):
+    text = text.split("\n")
+    for index, line in enumerate(text):
+        if "tema" in line.lower():
+            text = text[index:]
+            break
+    
+    text = [line for line in text if line != "\n"]
+    exercises = []
+
+    topic = None
+    for line in text:
+        if "tema" in line.lower():
+            topic = line.split(":")[1].strip().lower()
+            topic = TOPICS_DICT[topic]
+        elif "Ju" in line or "Reserva" in line:
+            exercises.append(line[2:])  # remove " · "
+        else:
+            pass
+    if topic and exercises:
+        return topic, exercises
+
+if file:
+    reader = PdfReader(file)
+    first_page = reader.pages[0]
+    text = first_page.extract_text()
+    topic, exercises = parse_text(text)
+
+    # load exercise types for selected topic
+    exercise_types = None
+    try:
+        with open("exercise_types.json", "r", encoding="utf-8") as file:
+            exercise_types = json.load(file)
+    except FileNotFoundError:
+        print(f"El archivo {topic}.json, con clasificaciones de ejercicios, no existe.")
+
+    st.subheader(f"Tema: {topic.title()}")
+
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        
+        selected_exercise = st.selectbox(
+            label = "Examen:",
+            options= exercises,
+        )
+
+        if selected_exercise:
+            exam, exercise = selected_exercise.split(", ")
+
+    with col2:
+        if exercise_types:
+            ex_type = st.selectbox(
+                label = "Tipo de ejercicio",
+                options = exercise_types[topic]
+            )
+
+    st.text(f"Convocatoria: {exam} \t|\t Ejercicio: {exercise} \t|\t Tipo ejercicio: {ex_type}")
