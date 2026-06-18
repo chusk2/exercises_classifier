e2a1ccf15d4661ccb730fa64dd7c53d6fc79a7d8
Daniel <daniel@192.168.1.9>
2026-06-18 14:09:06 +0200

    Create app with filters, selectbox for exercises and types, as well as new exercise input widget

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ import sqlite3
 from pyuca import Collator
 
 
-st.set_page_config(layout = "centered", page_title = "Exam Classifier")
+st.set_page_config(layout = "wide", page_title = "Exam Classifier")
 st.title("Exam Classifier")
 
-with open("topics.json", mode = "r", encoding = "utf-8") as file:
-    topics_dict = json.load(file)
+with open("exercise_types.json", mode = "r", encoding = "utf-8") as file:
+    exercise_types_dict = json.load(file)
 
 # create connection to database
 conn = sqlite3.connect("database.db")
@@ -69,5 +69,50 @@ selected_exercise = st.selectbox(
         options= list(exercises_df.combined_ex)
     )
 
+available_exercise_types = exercise_types_dict.get(selected_topic)
+selected_exercise_type = None
+exercise_type = None
 
-st.dataframe(exercises_df)
+if available_exercise_types:
+    selected_exercise_type = st.selectbox(
+            label = "Tipo de ejercicio:",
+            options= available_exercise_types
+        )
+
+new_exercise_type = st.text_input(label = "Nuevo tipo de ejercicio",
+                                  value="",
+                                  max_chars=100,
+                                  placeholder="Inserta nuevo tipo de ejercicio...",
+                                  type="default"
+)
+
+
+if new_exercise_type:
+    exercise_type = new_exercise_type
+elif selected_exercise_type:
+    exercise_type = selected_exercise_type
+
+convocatoria, ejercicio = selected_exercise.split(" - ")
+
+data = {
+    "asignatura" : selected_subject,
+    "tema" : selected_topic,
+    "año" : selected_year,
+    "convocatoria" : convocatoria,
+    "ejercicio" : ejercicio,
+    # if a new exercise type is provided
+    "tipo_ejercicio" : exercise_type
+}
+
+if st.button("Guardar datos"):
+    data_df = pd.DataFrame([data])
+    data_df.to_sql("ejercicios", conn, if_exists="append", index = False)
+    conn.commit()
+    saved_data_string = ""
+    for key, value in data.items():
+        saved_data_string += f"**{key}**: *{value}*  \n" 
+
+    st.success(f"### Datos guardados:  \n{saved_data_string}")
+
+
+st.dataframe(exercises_df, hide_index = True, use_container_width=True)
