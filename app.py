a62b2e9ddd2a14962219ef1c93121886f1242a51
Daniel <daniel@192.168.1.9>
2026-06-18 13:25:35 +0200

    Create basic layout showing filters and list of exercises

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 from pypdf import PdfReader
 import json
 import pandas as pd
+import sqlite3
+from pyuca import Collator
+
 
 st.set_page_config(layout = "centered", page_title = "Exam Classifier")
 st.title("Exam Classifier")
@@ -9,81 +12,62 @@ st.title("Exam Classifier")
 with open("topics.json", mode = "r", encoding = "utf-8") as file:
     topics_dict = json.load(file)
 
-file = st.file_uploader(label="Selecciona un archivo...", type = ["pdf"])
+# create connection to database
+conn = sqlite3.connect("database.db")
 
-def extract_info_from_pdf(pdf_file):
-    reader = PdfReader(pdf_file)
-    text = reader.pages[0].extract_text()
-    text_lines = [line.strip() for line in text.split("\n") if line.strip()][3:]
+# get the dataframe from all available data in db
+df = pd.read_sql("select * from ejercicios ;", conn)
+
+
+col1, col2 = st.columns(2)
     
-    return text_lines
-
-def parse_lines(text_lines):
+with col1:
     
-    year = int(text_lines[0])
-    subject = text_lines[1].lower()
-    topic = text_lines[2].split(":")[1].strip().lower()
-    topic = topics_dict[topic]
+    # select subject
+    collator = Collator()
+    subjects = sorted( list(df.asignatura.unique()), key=collator.sort_key )
 
-    # define the data dictionary
-    data = {
-           "year" : year,
-           "subject" : subject,
-           "topic" : topic,
-           "exercises" : []
-    }
+    selected_subject = st.selectbox(
+        label = "Asignatura:",
+        options= subjects
+    )
 
-    for line in text_lines[3:]:
-        line = line[2:]  # remove "• "
-        components = line.split(', ')
-        data["exercises"].append((components[0], ', '.join(components[1:]) ) )
-    
-    return data
+    # select topic based on selected subject
+    if selected_subject:
+        topics = df.loc[df.asignatura == selected_subject, "tema"].unique()
+        topics = sorted(list(topics), key=collator.sort_key)
 
+    selected_topic = st.selectbox(
+        label = "Tema:",
+        options= topics
+    )
 
-if file:
-    
-    text_lines = extract_info_from_pdf(file)
-    data = parse_lines(text_lines)
+    # select year based on selected subject and topic
+    if selected_topic and selected_subject:
+        mask = (df.asignatura == selected_subject) & (df.tema == selected_topic) 
+        years = df.loc[mask, "año"].unique()
+        years = sorted(list(years))
 
-    # load exercise types for selected topic
-    exercise_types = None
-    try:
-        with open("exercise_types.json", "r", encoding="utf-8") as file:
-            exercise_types = json.load(file)
-    except FileNotFoundError:
-        print(f"El archivo {data["topic"]}.json, con clasificaciones de ejercicios, no existe.")
-
-    st.subheader(f"Tema: {data["topic"].title()}")
-
-    col1, col2 = st.columns(2)
-    
-    with col1:
-        
-        selected_exercise = st.selectbox(
-            label = "Examen:",
-            options= [ex[0] + " - " + ex[1] for ex in data["exercises"] ]
+        selected_year = st.selectbox(
+            label = "Año:",
+            options= years
         )
 
-        if selected_exercise:
-            exam, exercise = selected_exercise.split(" - ")
 
-    with col2:
-        if exercise_types:
-            ex_type = st.selectbox(
-                label = "Tipo de ejercicio",
-                options = exercise_types[data["topic"]]
-            )
-    
-    current_exercise = {
-        "asignatura" : data["subject"],
-        "tema" : data["topic"],
-        "año" :data["year"],
-        "convocatoria" : exam,
-        "ejercicio" : exercise,
-        "tipo_ejercicio" : ex_type
-        }
-    df = pd.DataFrame([current_exercise])
-    
-    st.dataframe(df.T, use_container_width=True)
-    
+mask = (df.asignatura == selected_subject) & \
+(df.tema == selected_topic) & \
+(df.año == selected_year)
+
+exercises_df = df[mask].copy()
+exercises_df["combined_ex"] = (exercises_df
+                                .apply(lambda row: row.loc["convocatoria"] + " - " +
+                                        row.loc["ejercicio"], axis=1)
+)
+
+selected_exercise = st.selectbox(
+        label = "Ejercicio:",
+        options= list(exercises_df.combined_ex)
+    )
+
+
+st.dataframe(exercises_df)
